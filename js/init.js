048564513e86a5a05e55d7c69ab42ae753c6f058
nsofina <sonata-24@yandex.ru>
2022-04-06 16:30:00 +0300

    add style

--- js/init.js
+++ js/init.js
@@ -1,6 +1,9 @@
+window.onload = createPerson();
 
-window.onload = function()
-{
+const creater = document.getElementById('creater');
+const deleter = document.getElementById('deleter');
+
+function createPerson() {
     const initPerson = personGenerator.getPerson();
     document.getElementById('firstNameOutput').innerText = initPerson.firstName;
     document.getElementById('surNameOutput').innerText = initPerson.surName;
@@ -9,3 +12,16 @@ window.onload = function()
     document.getElementById('profOutput').innerText = initPerson.prof;
     document.getElementById('birthYearOutput').innerText = initPerson.birthday;
 };
+
+function deletePerson() {
+    document.getElementById('firstNameOutput').innerText = '';
+    document.getElementById('surNameOutput').innerText = '';
+    document.getElementById('patrNameOutput').innerText = '';
+    document.getElementById('genderOutput').innerText = '';
+    document.getElementById('profOutput').innerText = '';
+    document.getElementById('birthYearOutput').innerText = '';
+}
+
+creater.addEventListener('click', createPerson);
+deleter.addEventListener('click', deletePerson);
+
